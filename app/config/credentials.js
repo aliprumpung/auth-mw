07f623a5c5d5c42d8d624d60d0584b15dde9ad43
aliprumpung <callmesike@gmail.com>
2018-03-12 18:38:38 +0700

    fix env

--- app/config/credentials.js
+++ app/config/credentials.js
@@ -2,12 +2,13 @@ module.exports = {
 	'facebookAuth' : {
 		'clientID': process.env.FACEBOOK_ID,
 		'clientSecret': process.env.FACEBOOK_SECRET,
-		'callbackURL': 'https://auth-mw.herokuapp.com/auth/facebook/callback'
+		'callbackURL': process.env.FACEBOOK_CALLBACKURL
 	},
 
 	'googleAuth' : {
 		'clientID': process.env.GOOGLE_ID,
 		'clientSecret': process.env.GOOGLE_SECRET,
-		'callbackURL': 'https://auth-mw.herokuapp.com/auth/google/callback'
+		'callbackURL':  process.env.GOOGLE_CALLBACKURL
 	}
 }
+// 'callbackURL': 'https://auth-mw.herokuapp.com/auth/facebook/callback'
